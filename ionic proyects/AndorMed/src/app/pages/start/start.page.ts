1164e11da5a0053c08cd634a965ff33dd4710b0d
JeftePlayGame <2102195@utrivieramaya.edu.mx>
2023-06-22 23:35:35 -0500

    mejoras

--- ionic proyects/AndorMed/src/app/pages/start/start.page.ts
+++ ionic proyects/AndorMed/src/app/pages/start/start.page.ts
@@ -10,19 +10,4 @@ export class StartPage {
 
   constructor(private router: Router) {}
 
-  goToAgenda() {
-    this.router.navigate(['/agenda']); // Reemplaza '/agenda' con la ruta real hacia la interfaz de agenda
-  }
-
-  goToPatients() {
-    this.router.navigate(['/patients']); // Reemplaza '/patients' con la ruta real hacia la interfaz de pacientes
-  }
-
-  goToReminders() {
-    this.router.navigate(['/reminders']); // Reemplaza '/reminders' con la ruta real hacia la interfaz de recordatorios
-  }
-
-  goToAnotherPage() {
-    this.router.navigate(['/another-page']); // Reemplaza '/another-page' con la ruta real hacia la interfaz de otra página
-  }
 }
