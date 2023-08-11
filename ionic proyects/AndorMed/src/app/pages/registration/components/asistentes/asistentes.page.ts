c54b467b0245743a31f8059ba7a9b7f42360cab6
JLHMV19 <jorgeluishm3456@gmail.com>
2023-08-10 19:35:44 -0500

    siu

--- ionic proyects/AndorMed/src/app/pages/registration/components/asistentes/asistentes.page.ts
+++ ionic proyects/AndorMed/src/app/pages/registration/components/asistentes/asistentes.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../../services/auth.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-asistentes',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asistentes.page.scss'],
 })
 export class AsistentesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  asistenteForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService,
+    private navCtrl: NavController
+  ) { 
+    this.asistenteForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+    });
   }
 
+  ngOnInit() {}
+
+  saveDetails() {
+    if (this.asistenteForm.valid) {
+      this.handleSaveDetails();
+      this.navCtrl.navigateRoot('/sucess');
+    } else {
+      this.markFormGroupTouched(this.asistenteForm);
+    }
+  }
+
+  handleSaveDetails() {
+    const formData = {
+      nombreDoctor: this.asistenteForm.value.name,
+      usuarios_idusuarios: this.authService.getUserId(), // Cambia esto por el método correcto para obtener el ID del usuario
+    };
+}
+
+markFormGroupTouched(formGroup: FormGroup) {
+  Object.values(formGroup.controls).forEach((control) => {
+    control.markAsTouched();
+  });
+}
 }
